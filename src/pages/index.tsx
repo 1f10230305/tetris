25586338aacb40ae30adf2e786a149bcb8ae184f
1f10230305 <s1f102303057@iniad.org>
2023-05-13 20:17:31 +0900

    create: index.tsx etc.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { Board } from '../components/Board/Board';
+import { Hover } from '../components/Hover/Hover';
+import { Information } from '../components/Information/Information';
+import { Next } from '../components/Next/Next';
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container} />;
+  return (
+    <div className={styles.container}>
+      <Hover />
+      <Information />
+      <Board />
+      <Next />
+    </div>
+  );
 };
 
 export default Home;
