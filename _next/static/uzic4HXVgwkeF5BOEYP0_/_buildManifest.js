83cf909d1d94773980d84913cf71ef5f33a86024
mst-mkt <mst-mkt@users.noreply.github.com>
2023-05-26 08:00:46 +0000

    deploy: 8be9d0fe8f3bbb9f7081e83986836adf4fbbdc81

--- _next/static/uzic4HXVgwkeF5BOEYP0_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f8d2f0749b54d256.css","static/chunks/pages/index-40a18133c8d44e53.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
