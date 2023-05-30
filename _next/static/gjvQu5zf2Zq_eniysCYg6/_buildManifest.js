4d7ba644a74e252585c7090449d52e05fcdbb294
mst-mkt <mst-mkt@users.noreply.github.com>
2023-05-30 16:35:54 +0000

    deploy: dc90862aa85f4f0e8423d89fa9ad7e51890868dd

--- _next/static/gjvQu5zf2Zq_eniysCYg6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/356bfa5a418a21b1.css","static/chunks/pages/index-13ed6d3451a0ad69.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/jOSm8cOJkGHFa6CKSMO2W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/356bfa5a418a21b1.css","static/chunks/pages/index-d9b03a57ec85e338.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
